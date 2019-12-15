1a6c826a7585b9f93a2793fd8e7b6a724c774383
Christian Celora <christian.celora@gmail.com>
2019-12-15 18:03:59 +0100

    Retrive Stock Info

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys 
 import os
 import json
-import request
+import requests
 
 def getAlphaVantageKey() -> str:
     script_dir = os.path.dirname(__file__) #absolute dir the script is in
@@ -13,13 +13,23 @@ def getAlphaVantageKey() -> str:
     if not "key" in json_data:
         raise Exception("chiave AlphaVantage non trovata")
     #get key
-    print(json_data)
     alpha_key = json_data["key"]
+
     return alpha_key
 
+def getStockInfo(key: str, stock: str):
+    params = {"function": "TIME_SERIES_INTRADAY", "symbol": stock, "interval": "5min", "apikey": alpha_key}
+    res = requests.get("https://www.alphavantage.co/query", params=params)
+    if res.status_code != 200:
+        raise Exception("errore", res.status_code)
+    return res.json()
+
+
 def main():
+    stock = "SRPT" #mock
     alpha_key = getAlphaVantageKey()
-    print("AlphaVantage key:", alpha_key)
+    res_data = getStockInfo(alpha_key, stock)
+    print(res_data)
     sys.exit(1)
 
 if __name__ == "__main__":
