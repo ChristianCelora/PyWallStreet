2cf69a957391285b7a543664322cffb49450aebb
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-13 18:26:53 +0100

    Added test strategy

--- test/test_strategy.py
+++ test/test_strategy.py
@@ -1,13 +1,42 @@
 import pytest
-from include.strategy import Strategy
+from datetime import datetime, timedelta
+from include.strategy import Strategy, Stock
 
 # INIT
-test_stock = Strategy("TEST", 10, 5)
+MINUTES_PERIOD = 5
+test_stock = Strategy("TEST", 10, MINUTES_PERIOD)
 
-def test_first():
-    assert 2 == 2
-
-def test_calcKPercent():
+def test_calc_k_percent():
     k_perc = test_stock.calcKPercent(100, 111, 86)
     expected = (100 - 86)/(111 - 86) * 100
     assert k_perc == expected
+
+def test_get_low_high():
+    # init
+    high = 0
+    low = 1000
+    now = datetime(2020, 2, 13, 14, 0, 0)   # date choosen is irrelevant
+    timestamp = now
+    for i in range(0,5):
+        state_high = 100 + 5*i
+        state_low = 100 - 5*i
+        stock_data = {
+            Stock.OPEN_INDEX: 100,
+            Stock.HIGH_INDEX: state_high,
+            Stock.LOW_INDEX: state_low,
+            Stock.CLOSE_INDEX: 100,
+            Stock.VOLUME_INDEX: 100,
+        }
+        test_stock.addData(timestamp.strftime("%Y-%m-%d %H:%M:%S"), stock_data)
+        if state_low < low:
+            low = state_low
+        if state_high > high:
+            high = state_high
+        if i < 4:   # skip last
+            timestamp = timestamp + timedelta(minutes=MINUTES_PERIOD)
+    # test
+    real = test_stock.getLowHigh(timestamp, now)
+    assert low == real["low"]
+    assert high == real["high"]
+
+    
