bfa252c4fee1ade53978047447080eaea2847b54
christian.celora@gmail.com <christian.celora@gmail.com>
2020-03-23 20:43:47 +0100

    all test passing

--- include/strategy.py
+++ include/strategy.py
@@ -28,8 +28,8 @@ class AbstractStrategy(ABC):
         self.periods = periods
         self.min_interval = inter
         self.name = stock
-        self.data = []
-        self.timestamps = []
+        self.bars = []
+        self.timestamps = []    # Keep track if the bar is alredy added
 
     def getDatetimeFromStr(self, date_str: str) -> datetime:
         return datetime.strptime(date_str, self.DATE_FORMAT)
@@ -45,37 +45,37 @@ class Strategy(AbstractStrategy):
 
     def addData(self, time: str, stock_data: dict):
         if not time in self.timestamps:
-            self.data.append(Stock(self.name, time, stock_data))
-            self.timestamps.append(time)
+            self.bars.append(Stock(self.name, time, stock_data))
+            self.timestamps.append([time])
             return True
         return False
 
     def action(self) -> int:
-        if len(self.data) > self.periods:  # Do nothin until you reach min periods 
+        if len(self.bars) > self.periods:  # Do nothin until you reach min periods 
             stoc_index = self.getStochasticIndex()
             mov_avg = self.getMovingAverage()
-            print("K%:", stoc_index["%K"], "mov_avg:", mov_avg, "last_close:", self.data[-1].close )
-            if stoc_index["%K"] >= 80 and mov_avg > self.data[-1].close:
+            print("K%:", stoc_index["%K"], "mov_avg:", mov_avg, "last_close:", self.bars[-1].close )
+            if stoc_index["%K"] >= 80 and mov_avg > self.bars[-1].close:
             #if stoc_index["%K"] >= 80:
                 return -1
-            elif stoc_index["%K"] <= 20 and mov_avg < self.data[-1].close:
+            elif stoc_index["%K"] <= 20 and mov_avg < self.bars[-1].close:
             #elif stoc_index["%K"] <= 20:
                 return 1
         return 0
 
     def getMovingAverage(self) -> float:
-        if len(self.data) < self.periods:
+        if len(self.bars) < self.periods:
             return -1
         sma = 0
         # get only last n-th timestamps (n = number periods)
-        for bar in self.data[-self.periods:]:
+        for bar in self.bars[-self.periods:]:
             sma += bar.close
         print("tot_sma", sma)
         return round(sma / self.periods, 2)
 
     def getStochasticIndex(self) -> dict:
         #calculate start period and end period
-        last_bar = self.data[-1]
+        last_bar = self.bars[-1]
         limits = self.getLowHigh()
         print(limits)
         #calculate stock %K
@@ -86,14 +86,15 @@ class Strategy(AbstractStrategy):
         return {"%K": perc_k, "%D": perc_d}  
 
     def getLowHigh(self) -> dict:
-        low = self.data[-self.periods].low
-        high = self.data[-self.periods].high
+        low = self.bars[-self.periods].low
+        high = self.bars[-self.periods].high
 
-        for bar in self.data[-(self.periods-1)]: 
-            if bar.low < low:
-                low = bar.low
-            if bar.high > high:
-                high = bar.high
+        start = (self.periods-1)*-1
+        for i in range(start, len(self.bars)): 
+            if self.bars[i].low < low:
+                low = self.bars[i].low
+            if self.bars[i].high > high:
+                high = self.bars[i].high
 
         return {"low": low, "high": high}
 
@@ -101,8 +102,3 @@ class Strategy(AbstractStrategy):
         if low == high:
             return 50
         return (closing_price - low) / (high - low) * 100
-
-    """def isUptrending(self) -> bool:
-        keys = list(self.data.keys())
-        return ( self.data[keys[0]].open - self.data[keys[len(keys)-1]].close < 0 )
-    """
